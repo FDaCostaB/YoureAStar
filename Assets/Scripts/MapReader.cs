0f526b289db9d0194ef2275cd3995f3b46641b7d
YargonIV <fabien.da-costa-barros@etu.univ-grenoble-alpes.fr>
2023-01-29 22:39:10 +0100

    Map update

--- Assets/Scripts/MapReader.cs
+++ Assets/Scripts/MapReader.cs
@@ -32,16 +32,23 @@ public class MapReader {
 				for (int x=0; x<line.Length; x++) {
 					switch(line[x]) {
 						case '#':
+						case '@':
+						case 'O':
+						case 'T':
 							m.addWall(x,i);
 							break;
-						case '@':
+						case 'X':
 							m.addCharacter(x,i);
 							break;
 						case '%':
+						case 'W':
 							m.addWater(x,i);
 							break;
 						case ' ':
 						case '$':
+						case '.':
+						case 'G':
+						case 'S':
 							m.add(0,x,i);
 							break;
 						default:
